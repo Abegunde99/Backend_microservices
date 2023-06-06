38593180c8b9928774e827cae8a900d69d625b6f
Abegunde99 <preciousolanrewaju1998@gmail.com>
2023-06-06 08:25:06 +0100

    I connected to database of each services

--- gateway/index.js
+++ gateway/index.js
@@ -6,9 +6,9 @@ const proxy = require('express-http-proxy');
 app.use(express.json());
 app.use(cors());
 
-app.use('/customers', proxy('http://localhost:3000'));
-app.use('/', proxy('http://localhost:3001'));
-app.use('/shopping', proxy('http://localhost:3002'));
+app.use('/shopping', proxy('http://localhost:3002')); //shopping
+app.use('/customer', proxy('http://localhost:3000')); //customer
+app.use('/', proxy('http://localhost:3001')); //products
 
 
 app.listen(3003, () => {
