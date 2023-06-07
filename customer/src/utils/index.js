809cb39a118dbfe3bbd1978ce1369e863f913af2
Abegunde99 <preciousolanrewaju1998@gmail.com>
2023-06-07 10:33:16 +0100

    I completed the custoner service and started the product service

--- customer/src/utils/index.js
+++ customer/src/utils/index.js
@@ -31,8 +31,9 @@ module.exports.GenerateSignature = async (payload) => {
 
 module.exports.ValidateSignature = async (req) => {
   try {
-    const signature = req.get("Authorization");
-    console.log(signature);
+    // const signature = req.headers.get("Authorization");
+    const { authorization: signature } = req.headers;
+    // console.log(signature);
     const payload = await jwt.verify(signature.split(" ")[1], APP_SECRET);
     req.user = payload;
     return true;
